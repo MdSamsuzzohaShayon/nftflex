0264ce5ae8713b51454f277063a598edcdebb622
Md Shayon <mdshayon0@gmail.com>
2025-01-22 17:23:43 +0600

    Scripting and testing with APE

---
+++ nftflex-smart-contract/scripts/deploy.py
@@ -0,0 +1,7 @@
+# Docs -> https://docs.apeworx.io/ape/latest/userguides/scripts.html
+
+import click
+
+@click.command()
+def cli():
+    print("Hello world!")
---
+++ nftflex-smart-contract/tests/test_NFTFlex.py
@@ -0,0 +1,3 @@
+# Docs -> https://docs.apeworx.io/ape/latest/userguides/testing.html
+def test_add():
+    assert 1 + 1 == 2
